bdfdb9193c80f73a449df11476532441c87dce1d
xinyu001 <55417154+xinyu001@users.noreply.github.com>
2020-06-02 16:01:18 +0800

    first commit

--- pages/functions/repair/repair.js
+++ pages/functions/repair/repair.js
@@ -1,16 +1,5 @@
 Page({
-  f0:function(event){
-    wx.showModal({
-      title: '提示',
-      content: '这是一个模态弹窗',
-      success: function (res) {
-        if (res.confirm) {//这里是点击了确定以后
-          console.log('用户点击确定')
-        } else {//这里是点击了取消以后
-          console.log('用户点击取消')
-        }}
-    })
-  },
+ 
   f1:function(event){
     wx.showModal({
       title: '提示',
@@ -24,6 +13,4 @@ Page({
     })
   }
   
-
-
 })
